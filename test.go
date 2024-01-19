c85379589e9068bdffb29b0a2f4c0aac89b095f8
Umesh <usirsiwal@uptycs.com>
2024-01-19 15:47:08 -0500

    introduce redirect warning.

--- test.go
+++ test.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func sanitizeUrl(redir string) string {
+	if len(redir) > 0 && redir[0] == '/' {
+		return redir
+	}
+	return "/"
+}
+
 func controller(msg string) {
 	switch {
 	case msg == "start":
@@ -15,9 +22,9 @@ func controller(msg string) {
 }
 
 func main() {
+	u := "/abc/def"
+	sanitizeUrl(u)
 	controller("nope")
 	var p *int
 	fmt.Printf("%v\n", *p)
 }
-
-
